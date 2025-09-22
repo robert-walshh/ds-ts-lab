a6702da16019700cba4c6feb88b2f4e2c8e8f72b
Robert Walsh <robertwalsh@tuta.com>
2025-09-22 12:25:25 +0100

    Type aliases demonstration.

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -1,27 +1,49 @@
 export interface Friend {
     name: string;
     phone: string;
-    dob? : Date;   // New
+    dob?: Date;   // New
     age: number;
-    interests? : string[]   // New
+    interests?: string[]   // New
 }
 
 export interface Colleague {
     name: string;
     department: string;
     contact: {
-      email: string;
-      extension: number
-    } 
-  }
+        email: string;
+        extension: number
+    }
+}
 
 export interface ColleagueHistory {
-  current: Colleague[],
-  former: Colleague[]
+    current: Colleague[],
+    former: Colleague[]
 }
 
 export interface EmailContact {
-  name: string;
-  email: string
+    name: string;
+    email: string
 }
+
+export type Department = "Engineering" | "Finance" | "HR";
+
+export interface ColleagueV2 {
+    name: string;
+    department: Department;    // *****
+    contact: {
+        email: string;
+        extension: number;
+        slack?: string;
+    };
+}
+
+export type Buddy = Friend | ColleagueV2;
+export type Administrator = Buddy | string | undefined
+
+export type BuddyList = {
+    name: string;
+    administrator: Administrator;
+    members: Buddy[];
+};
+
 

28ebfa942c64269e22e5282003dc92c4510d21b9
Robert Walsh <robertwalsh@tuta.com>
2025-09-22 11:54:20 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -45,19 +45,20 @@ console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 function sortColleagues(
     colleagues: Colleague[],
     sorter: (c1: Colleague, c2: Colleague) => number,
-    max? : number
+    max?: number
 ): EmailContact[] {
     let end = colleagues.length;
     if (max !== undefined) {
         end = max < 2 ? 1 : max
     }
     const sorted = colleagues.sort(sorter);
-    const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
-    return fullResult.slice(0,end)
+    const fullResult = sorted.map((ce) => ({name: ce.name, email: ce.contact.email}));
+    return fullResult.slice(0, end)
 }
+
 // Test invocations
-console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
-console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension), 3));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length), 1));
 console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
 
 
@@ -72,4 +73,17 @@ function findFriends(
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Ro')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
 
+function addInterest(
+    friend: Friend,
+    interest: string
+): string[] {
+    if (!friend.interests) {
+        friend.interests = [];
+    }
+
+    friend.interests.push(interest);
+    return friend.interests
+}
+
+console.log(addInterest(friends[0], 'Politics'))
 
